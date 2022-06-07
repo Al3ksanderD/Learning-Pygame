83f0ee51164b05287f823ee332d4b0f88bfa49c4
Aleks <olo1045@wp.pl>
2022-06-07 20:44:53 +0200

    Basic functionality

--- main.py
+++ main.py
@@ -3,4 +3,41 @@ import pygame
 # Initialize the pygame
 pygame.init()
 
-screen = pygame.display.set
+# create the screen
+screen = pygame.display.set_mode((800, 600))
+
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("ufo.png")
+pygame.display.set_icon(icon)
+
+# Player
+player_image = pygame.image.load("arcade-game.png")
+playerX = 370
+playerY = 480
+
+
+def player(x, y):
+    screen.blit(player_image, (x, y))
+
+
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                print("Left arrow pressed")
+            if event.key == pygame.K_RIGHT:
+                print("Right arrow pressed")
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                print("Left arrow released")
+            if event.key == pygame.K_RIGHT:
+                print("Right arrow released")
+    screen.fill((148, 87, 235))
+
+    player(playerX, playerY)
+    pygame.display.update()
